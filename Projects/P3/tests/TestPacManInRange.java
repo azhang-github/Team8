48ec1b3e6d68d2f7090c2f7002abce2da06e792a
Kaiū <cyl@umd.edu>
2022-03-03 11:45:13 -0500

    fix indentation

--- Projects/P3/tests/TestPacManInRange.java
+++ Projects/P3/tests/TestPacManInRange.java
@@ -8,11 +8,11 @@ import static org.junit.Assert.assertFalse;
 public class TestPacManInRange extends TestCase {
 
 	public void testPacManInRange() throws FileNotFoundException{
-    NoFrame frame = new NoFrame();
-    PacMan pacman = frame.addPacMan(new Location(2, 2));
-    Ghost ghost1 = frame.addGhost(new Location(2, 1), "ghost1", Color.red);
-    assertTrue(ghost1.is_pacman_in_range());
-    Ghost ghost2 = frame.addGhost(new Location(4, 4), "ghost2", Color.red);
-    assertFalse(ghost2.is_pacman_in_range());
+	    NoFrame frame = new NoFrame();
+	    PacMan pacman = frame.addPacMan(new Location(2, 2));
+	    Ghost ghost1 = frame.addGhost(new Location(2, 1), "ghost1", Color.red);
+	    assertTrue(ghost1.is_pacman_in_range());
+	    Ghost ghost2 = frame.addGhost(new Location(4, 4), "ghost2", Color.red);
+	    assertFalse(ghost2.is_pacman_in_range());
 	}
 }
