2421ec6e256caa972b78f12c98077464ce4c21c9
Michael Li <mli25782@umd.edu>
2022-03-04 20:43:25 -0500

    fixed ghost and pacman move tests

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -6,14 +6,14 @@ public class TestGhostMove extends TestCase {
 
 	public void testGhostMove() throws FileNotFoundException{
 		NoFrame frame = new NoFrame();
-		Ghost ghost = frame.addGhost(new Location(9,11), "binky", Color.red);	
+		Ghost ghost = frame.addGhost(new Location(1,1), "binky", Color.red);
 		assertTrue(ghost.move());
-	
-		Location loc1 = new Location(9,12);
-		Location loc2 = new Location(10,11);
-		Location loc3 = new Location(10,12);
 
-		System.out.println(ghost.myLoc.x + " " + ghost.myLoc.y);		
+		Location loc1 = new Location(1,2);
+		Location loc2 = new Location(2,1);
+		Location loc3 = new Location(2,2);
+
+		System.out.println(ghost.myLoc.x + " " + ghost.myLoc.y);
 		assertTrue(ghost.myLoc.equals(loc1) || ghost.myLoc.equals(loc2) || ghost.myLoc.equals(loc3));
 	}
 }
--- Projects/P3/tests/TestPacManMove.java
+++ Projects/P3/tests/TestPacManMove.java
@@ -6,12 +6,12 @@ public class TestPacManMove extends TestCase {
 
 	public void testPacManMove() throws FileNotFoundException{
 		NoFrame frame = new NoFrame();
-		PacMan pacman = frame.addPacMan(new Location(9,11));
+		PacMan pacman = frame.addPacMan(new Location(1,1));
 		assertTrue(pacman.move());
-	
-		Location loc1 = new Location(9,12);
-		Location loc2 = new Location(10,11);
-		Location loc3 = new Location(10,12);
+
+		Location loc1 = new Location(1,2);
+		Location loc2 = new Location(2,1);
+		Location loc3 = new Location(2,2);
 
 		assertTrue(pacman.myLoc.equals(loc1) || pacman.myLoc.equals(loc2) || pacman.myLoc.equals(loc3));
 	}
